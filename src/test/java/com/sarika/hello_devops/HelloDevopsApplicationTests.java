b1733ad1e7f207fbdbf6609a3bfc73221aa5653b
Sarika Patel <SARIKA@Pavans-MacBook-Pro.local>
2026-05-19 17:34:56 -0400

    Initial Spring Boot REST API project

---
+++ src/test/java/com/sarika/hello_devops/HelloDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarika.hello_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
